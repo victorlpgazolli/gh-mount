60131c74af62a868db46a52b16205def19aac1e2
victorlpgazolli <victorlpgazolli@gmail.com>
2022-12-27 14:15:34 -0300

    ref build sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,6 @@
 
 mkdir -p ./dist
 
-TARGET_TRIPLE=${TARGET_TRIPLE:-x86_64-unknown-linux-gnu}
-GOOS_GOARCH=${GOOS_GOARCH:-linux-amd64}
+cargo build --release --locked
 
-cargo build --release --locked --target "${TARGET_TRIPLE}"
-strip "target/${TARGET_TRIPLE}/release/gh-mount"
-mv "target/${TARGET_TRIPLE}/release/gh-mount" "./dist/${GOOS_GOARCH}"
+mv target/release/gh-mount "./dist/linux-amd64"
